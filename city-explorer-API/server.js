e43c91345d507276e474cd86713153282925729f
bushraaljafari <bushra.aljafari@gmail.com>
2021-08-04 00:59:52 +0300

    push part2 lab7

--- city-explorer-API/server.js
+++ city-explorer-API/server.js
@@ -6,31 +6,37 @@ const cors=require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT;
 app.use(cors());
+app.get('/',(req,res)=>{
+    res.send('hello');
+});
 //---1
+
 app.get('/weather',(req,res)=>{
-    const reqCity=req.query.cityName;
+    const {searchQuery,lat,lon}=req.query;//disstracturing assigment---to assign value in obj
+   // const reqCity=req.query.cityName;
     const availableCityData=weatherData.find(element=>{
-          if(reqCity.toLowerCase()===element.city_name.toLowerCase()){
-              return element;
-          }
-          res.status(500).send('sorry :we dont have this city')  
-      }  
+     if  (element.city_name.toLowerCase() ===searchQuery.toLowerCase()||(`${element.lat}`===lat
+       &&`${element.lon}`===lon)){
+           return element;
+       }
+        
+    } 
     );
 //---4
-    const chosenCity=availableCityData.data.map(element=>{
-        return new Forecast(element);
+    const chosenCity=availableCityData.data.map(value=>{
+        return new Forecast(value);
     }
         );
         //----5
         res.send(chosenCity);
-       
+        res.status(500).send('sorry :we dont have this city');
 
 
 })
 //---3
 class Forecast{
     constructor(item){
-        this.description= item.weather.description;
+        this.description= `${item.weather.description}`;
         this.date=item.valid_date;
         this.temp=item.temp;
         this.low_temp=item.low_temp;
@@ -39,9 +45,8 @@ class Forecast{
     }
 }
 /*app.get('/error',(req,res)=>{
-    res.status(500).send('sorry :we dont have this city')
-}
-)*/
+    res.status(500).send('sorry :we dont have this city');});*/
+
 //---2
 app.listen(PORT,()=>{console.log(`server start on ${PORT}`);})
 
