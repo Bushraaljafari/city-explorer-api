952f3724c2a6cc57e18689420d628cbe8be63671
bushraaljafari <bushra.aljafari@gmail.com>
2021-08-05 00:22:56 +0300

    push part 1 lab 8

--- city-explorer-API/server.js
+++ city-explorer-API/server.js
@@ -1,17 +1,20 @@
 'use strict';
 const express =require ('express');
 const app =express();
-const weatherData=require('./data/weather.json');
+//const weatherData=require('./data/weather.json');
+const axios = require('axios');
 const cors=require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT;
+const WEATHER_API_KEY=process.env.WEATHER_API_KEY; 
+const MOVIE_API_KEY=process.env.MOVIE_API_KEY;
 app.use(cors());
 app.get('/',(req,res)=>{
     res.send('hello');
 });
 //---1
 
-app.get('/weather',(req,res)=>{
+/*app.get('/weather',(req,res)=>{
 
     try{
     const {searchQuery,lat,lon}=req.query;//disstracturing assigment---to assign value in obj
@@ -46,10 +49,73 @@ class Forecast{
         this.max_temp=item.max_temp;
 
     }
+}*/
+//******************** */
+app.get('/weather',gettingWeather)
+app.get('/movies',gettingMovies)
+//--------
+function gettingWeather(req,res){
+    let {lat,lon}=req.query;
+    let weathUrlReq=`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lon}`
+     //await
+      axios.get(weathUrlReq)
+     .then(results=>{
+        let value=results.data.data[0];
+       // console.log('result.data.data',value);
+        let weatherData=new Forecast(value);
+        res.send(weatherData);
+
+    })
+    .catch(err=>{
+        res.status(500).send(`we have error on getting adata ${err}`)
+    })
 }
-/*app.get('/error',(req,res)=>{
-    res.status(500).send('sorry :we dont have this city');});*/
+
+function gettingMovies(req,res){
+    let reqMovies=req.query
+    let moviesulr=`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${reqMovies}`
+   // await 
+   axios.get(moviesulr)
+    .then(results=>{
+        let value=results.data.data[0];
+        let movies=new Movies (value);;
+        response.send(movies);
+    })
+    .catch(err=>{
+        res.status(500).send(` we have error on getting adata ${err}`)
+    })
+}
+
+     
+
+
+
+
+
+
+class Forecast{
+    constructor(item){
+        this.description= `"low of  ${item.min_temp} , high of ${item.max_temp} with ${item.weather.description}"`;
+        this.date=item.datetime;
+    }}
+
+
+    class Movies {
+        constructor(item){
+            this.title=item.original_title;
+            this.overview=item.overview;
+            this.votes=item.vote_count
+            // iam not sure about img 
+            this.img=item.poster_path;
+            this.popularity=item.popularity;
+           
+            this.released_on=item.release_date;
+        }
+    }
+
 
 //---2
+/////----------???????????
 app.listen(PORT,()=>{console.log(`server start on ${PORT}`);})
 
+////////
